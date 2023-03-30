7c6fd9cf9f9f9ea9f1c8596fd46fd8785b590ed8
Himanshu Gupta <hgupta+eGain@eGain.com>
2023-03-29 19:38:00 -0700

    Restructured and refactored.

--- java/src/FibSeq.java
+++ java/src/FibSeq.java
@@ -1,21 +1,31 @@
-
+import java.util.*;
 
 public class FibSeq {
+
+    private static Map<Integer, Long> data = new HashMap<>();
+
     public static void main(String[] args) {
         System.out.println("This is my Feb Seq for learning Dynamic Programming..");
 
-
-        int n = 10;
-        int an = getNthNumber(n + 2);
+        int n = 500;
+        long an = getNthNumber(n);
         System.out.println(n + "th number in my Feb Seq  -> " + an);
 
     }
 
-    private static int getNthNumber(int n) {
+    private static long getNthNumber(int n) {
+
+        if (data.containsKey(n))
+            return data.get(n);
 
         if (n == 1 || n == 2)
             return 1;
- 
-        return getNthNumber(n - 1) + getNthNumber(n - 2);
+
+        long x = getNthNumber(n - 1);
+        long y = getNthNumber(n - 2);
+
+        long z = x + y;
+        data.put(n, z);
+        return z;
     }
 }
--- python/fibSeq.py
+++ python/fibSeq.py
@@ -1,12 +1,26 @@
+
 def getNthNumber(n):
+
+    if n in data.keys():
+        return data[n];
+
     if n == 1 or n == 2:
         return 1
+    
+    a = n-1
+    b = n-2
 
-    return getNthNumber(n-1) + getNthNumber(n-2)
+    x = getNthNumber(a)
+    y = getNthNumber(b)
+    z = x + y
+    data[n] = z
+
+    return z
 
 
 print("This is my Feb Seq for learning Dynamic Programming..")
-n = 10
+n = 500
 
-an = getNthNumber(n+2)
+data = dict()
+an = getNthNumber(n)
 print(str(n) + "th number in my Feb Seq  -> " + str(an))
