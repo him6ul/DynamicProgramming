e9c955d5ee2ed734bcc495f07804c89ebfc9ba20
Himanshu Gupta <him6ul@gmail.com>
2023-04-02 18:33:08 -0700

    HowSum implmented

--- java/src/HowSum.java
+++ java/src/HowSum.java
@@ -1,18 +1,19 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CanSum {
+public class HowSum {
 
     private static Map<Integer, Boolean> resultMap = new HashMap<>();
 
     public static void main(String[] args) {
-        System.out.println("This is my can sum program..");
+        System.out.println("This is my how sum program..");
         int sum = 7000;
-        boolean result = canSum(sum, new int[] { 5, 3, 4, 7 });
-        System.out.println("Result for sum [" + sum + "] -> " + result);
+        int[] result = howSum(sum, new int[] { 5, 3, 4, 7 });
+        System.out.println("Result for sum [" + sum + "] -> " + Arrays.toString(result));
     }
 
-    private static boolean canSum(int sum, int[] args) {
+    private static int[] howSum(int sum, int[] args) {
 
         if (resultMap.containsKey(sum))
             return resultMap.get(sum);
@@ -24,7 +25,7 @@ public class CanSum {
 
         for (int key : args) {
             int diff = sum - key;
-            boolean result = canSum(diff, args);
+            boolean result = howSum(diff, args);
             if (result) {
                 resultMap.put(sum, result);
                 return true;
